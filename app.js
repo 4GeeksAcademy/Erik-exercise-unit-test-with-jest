ff8e92ad596df7db8d637beb8a4753e1958670fa
Erik Martin <erikmartinfernandez@gmail.com>
2024-11-06 19:08:22 +0000

    Entrega

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ let oneEuroIs = {
     "GBP": 0.87,  // British pound
 };
 
+//euro to dolar
 const fromEuroToDollar = function(valueInEuro) {
     let valueInDollar = valueInEuro * 1.07;
     return valueInDollar;
 }
 
+//dolar to yen
 const fromDollarToYen = function(valueInDolar) {
     let valueInEuro = valueInDolar / 1.07
     let valueInYen = valueInEuro * 156.5
     return valueInYen;
 }
 
+//yen to pound
 const fromYenToPound = function(valueInYen) {
     let valueInEuro = valueInYen / 156.5
     let valueInPound = valueInEuro * 0.87
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
 
-
+//euro to dolar
 test("One euro should be 1.07 dollars", function() {
     const { fromEuroToDollar } = require('./app.js');
     const dollars = fromEuroToDollar(3.5);
@@ -10,12 +10,14 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(expected); 
 })
 
+//dolar to yen
 test("One dolar should be 154,45 yen", function() {
     const yen = fromDollarToYen(1);
     const expected = (1 / 1.07) * 156.5;
     expect(yen).toBeCloseTo(expected);
 })
 
+//yen to pound
 test("One yen should be 0,005 british pound", function() {
     const pounds = fromYenToPound(1);
     const expected = (1 / 156.5) * 0.87;
